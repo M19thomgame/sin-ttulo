f9d855d34decf7888c93aa4eb6c27c77ed9b6381
M19thomgame <88722126+M19thomgame@users.noreply.github.com>
2021-09-24 09:24:59 -0500

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,98 @@
+function Feliz () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        # # # # #
+        `)
+}
+function Triste () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        # . . . #
+        `)
+}
+function Brava () {
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        # # . # #
+        . . . . .
+        # # # # #
+        `)
+}
+function temor () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        . # . # .
+        # . # . #
+        `)
+}
+function Seria () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # # # # #
+        . . . . .
+        `)
+}
+basic.forever(function () {
+    let quier_jugar = 0
+    if (quier_jugar == 0) {
+        Triste()
+    } else {
+    	
+    }
+    if (0 == 0) {
+        Triste()
+    } else {
+    	
+    }
+    if (0 == 0) {
+        Triste()
+    } else {
+    	
+    }
+    if (0 == 0) {
+        Triste()
+    } else {
+    	
+    }
+})
+basic.forever(function () {
+    let hidratacion = 0
+    let agua = 0
+    let dialogo = 0
+    let hablo = 0
+    let balon = 0
+    let jugar = 0
+    let iluminacion = 0
+    let sol = 0
+    if (sol == iluminacion) {
+        Feliz()
+    } else {
+    	
+    }
+    if (jugar == balon) {
+        Feliz()
+    } else {
+    	
+    }
+    if (hablo == dialogo) {
+        Feliz()
+    } else {
+    	
+    }
+    if (agua == hidratacion) {
+        Feliz()
+    } else {
+    	
+    }
+})
